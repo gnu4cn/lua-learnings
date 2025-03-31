9ed8bb6bc7060e9a3c0788e6141f79208db09788
Hector PENG <unisko@gmail.com>
2025-03-31 16:33:02 +0800

    Finished 'src/reflection.md'.

--- scripts/reflection/demo_call_hook.lua
+++ scripts/reflection/demo_call_hook.lua
@@ -10,13 +10,18 @@ local validfunc = {
     ...     -- 其他已授权函数
 }
 
+local memlimit = 1000
+local function checkmem ()
+    if collectgarbage("count") > memlimit then
+        error("脚本使用了太多内存")
+    end
+end
+
 local function hook (ev)
     print("Enter hook")
+
     if ev == "call" then
         local info = debug.getinfo(2, "fn")
-        local fn = validfunc[info.name]
-        print(fn)
-
         if not validfunc[info.func] then
             error("正调用不良函数：" .. (info.name or "?"))
         end
@@ -26,10 +31,13 @@ local function hook (ev)
     if count > steplimit then
         error("脚本使用了过多 CPU")
     end
+
+    checkmem()
 end
 
 -- 加载代码块
 local f = assert(loadfile(arg[1], "t", {}))
-debug.sethook(hook, "call", 100)    -- 设置钩子
+--- debug.sethook(hook, "", 100)    -- 设置钩子
+debug.sethook(hook, "")    -- 设置钩子
 f()     -- 运行代码块
 debug.sethook()
