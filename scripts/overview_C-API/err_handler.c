605c86904e02f143fb4f2631f48fab11bb586a8a
Hector PENG <unisko@gmail.com>
2025-04-04 17:32:07 +0800

    Updated 'src/overview_C-API.md'.

---
+++ scripts/overview_C-API/err_handler.c
@@ -0,0 +1,13 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void error (lua_State *L, const char *fmt, ...) {
+    va_list argp;
+    va_start(argp, fmt);
+    vfprintf(stderr, fmt, argp);
+    va_end(argp);
+    lua_close(L);
+    exit(EXIT_FAILURE);
+}
